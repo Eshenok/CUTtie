f1f7feed33ea4b70655c6f8e43e8cd1581a654f6
Eshenok <102781985+Eshenok@users.noreply.github.com>
2024-10-05 16:43:57 +0300

    save

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ img.src = image;
   })
 })
 
-// CutCropImg
+// CUTtie
 
 const resultSelf = document.getElementById('resultSelf');
 const demoSelf = document.getElementById('demoSelf')
@@ -108,15 +108,17 @@ class CutCropImg {
     this.maxH;
     this.canvas;
     this.ctx;
+    this.scale;
     this.canvasContainer;
   }
 
   _createCanvas() {
     this.canvas = document.createElement('canvas');
     this.canvasContainer = document.createElement('div');
-    this.canvasContainer.id = 'canvasCutCropImgDiv';
-    this.canvas.id = 'canvasCutCropImg';
+    this.canvasContainer.id = 'canvasCuttieDiv';
+    this.canvas.id = 'canvasCuttie';
     this.ctx = this.canvas.getContext('2d');
+    this.ctx.strokeStyle = 'lightgrey';
   }
 
   _appendStyles(params) {
@@ -138,15 +140,16 @@ class CutCropImg {
   }
 
   _addViewPort() {
-    this.ctx.strokeStyle = '#000';
-    this.ctx.strokeRect(this.viewport.x,this.viewport.y,this.viewport.w, this.viewport.h);
+    this.ctx.fillStyle = '#ffffff20'
+    this.ctx.fillRect(this.viewport.x,this.viewport.y,this.viewport.w, this.viewport.h);
     this.maxW = this.canvas.width - this.viewport.w;
-    this.maxH = this.canvas.height - this.viewport.h
+    this.maxH = this.canvas.height - this.viewport.h;
   }
 
   _onMouseDown(e) {
     e.preventDefault();
     e.stopPropagation();
+    this._findXY();
     this.viewport.isDraggable = true;
     const mx = parseInt(e.clientX - this.XY.offsetX);
     const my = parseInt(e.clientY - this.XY.offsetY);
@@ -172,9 +175,15 @@ class CutCropImg {
     this.viewport.y = Math.min(Math.max(0, this.viewport.y + dy), this.maxH);
 
     this._clearScene();
-    this.ctx.strokeRect(this.viewport.x,this.viewport.y, this.viewport.w, this.viewport.h);
+    this._addViewPort();
     }
 
+  }
+
+  _findXY(e) {
+    const bb = this.canvas.getBoundingClientRect();
+    this.XY.offsetX = bb.left;
+    this.XY.offsetY = bb.top;
   }
 
   _onMouseHover(e) {
@@ -200,6 +209,8 @@ class CutCropImg {
   _clearScene() {
     this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
     this.ctx.drawImage(this.image, 0,0,this.canvas.width,this.canvas.height);
+    this.ctx.fillStyle = '#00000040';
+    this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height);
   }
 
   _addEventListeners() {
@@ -207,10 +218,11 @@ class CutCropImg {
     this.canvas.onmouseup = this._onMouseUp.bind(this);
     this.canvas.onmousemove = this._onMouseMove.bind(this);
     this.canvas.onmouseleave = this._onMouseUp.bind(this);
+    this.canvas.onmouseenter = this._findXY.bind(this);
   }
 
   initCanvas(parentElem, params, url) {
-    const oldCanvasDiv = document.getElementById('canvasCutCropImgDiv');
+    const oldCanvasDiv = document.getElementById('canvasCuttieDiv');
     if (oldCanvasDiv) {
       parentElem.removeChild(oldCanvasDiv);
     }
@@ -227,20 +239,51 @@ class CutCropImg {
       this.canvas.width = drawWidth;
       this.canvas.height = drawHeight;
 
-      this.ctx.drawImage(this.image, 0, 0, drawWidth, drawHeight);
       this.viewport.h = params.viewport.height;
       this.viewport.w = params.viewport.width;
+      this._clearScene();
       this._addViewPort();
       this._addEventListeners();
-      const bb = this.canvas.getBoundingClientRect();
-      this.XY.offsetX = bb.left;
-      this.XY.offsetY = bb.top;
     }
+  }
+
+  getCrop(width, height) {
+    const canvas = document.createElement('canvas');
+    const sx = this.viewport.x/this.scale;
+    const sy = this.viewport.y/this.scale;
+    const sw = this.viewport.w/this.scale;
+    const sh = this.viewport.h/this.scale
+    canvas.width = width ? width : sw;
+    canvas.height = height ? height : sh;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(this.image, sx, sy, sw, sh,0,0,canvas.width,canvas.height);
+
+    return canvas.toDataURL('image/jpeg', 1.0);
+
   }
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //=========================================//
 //=========================================//
 //=========================================//
@@ -248,11 +291,11 @@ class CutCropImg {
 
 
 
-let cropper;
+let cuttie;
 uploadSelf.addEventListener('change', (e) => {
   const url = URL.createObjectURL(e.target.files[0]);
-  cropper = new CutCropImg()
-  cropper.initCanvas(
+  cuttie = new CutCropImg()
+  cuttie.initCanvas(
     demoSelf, 
     {
       bounds: {
@@ -261,10 +304,15 @@ uploadSelf.addEventListener('change', (e) => {
       },
       viewport: {
         type: 'square',
-        width: 150,
-        height: 100,
+        width: 200,
+        height: 112.5,
       }
     },
     url
   )
 });
+
+saveBtnSelf.addEventListener('click', () => {
+  const imaga = cuttie.getCrop(1920,1080);
+  resultSelf.src = imaga;
+})
